cf1686f8e265a31dd840c2a49b0000087c779953
Thomas Bonnardot <thomas.bonnardot@hei.fr>
2019-06-14 18:00:08 +0200

    Initial commit

---
+++ site-config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  // App Info
+  appstore_link         : "https://itunes.apple.com/fr/app/revelio-app/id1458707210",                                  // Enter App Store URL.
+  playstore_link        : null,                                  // Enter Google Play Store URL.
+  google_analytics_ID   : "UA-40324261-2",                                        // Enter Google Analytics ID or ""
+  presskit_download_link: "https://imedadel.me",                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
+  app_url               : "https://get-revelio-app.com",                            // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "Revelio",
+  app_price             : "Free",
+  app_description       : "Keep track of the movie you've seen.",
+  app_keywords          : ["movie", "theater", "revelio", "revelioapp", "automatic", "gatsbyjs", "app", "landing page"],
+
+  // Personal Info
+  your_name              : "Thomas Bonnardot",
+  your_link              : "https://get-revelio-app.com",
+  your_city              : "Paris, France",
+  email_address          : null,
+  linkedin_username      : null,
+  facebook_username      : null,
+  instagram_username     : "thomasbnnrdt",
+  twitter_username       : "tombonnardot",
+  github_username        : "tbnnrdt",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Watched",
+      description:
+        "Organize all the movies you have watched in one place.",
+      fontawesome_icon_name: "magic",
+    },
+    {
+      title: "Watchlist",
+      description:
+        "Easily remember which movie you want to see next.",
+      fontawesome_icon_name: "eye",
+    },
+    {
+      title: "Advanced search",
+      description:
+        "Find new movies to watch later.",
+      fontawesome_icon_name: "search",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.1)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
+  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "ffffff",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"Helvetica Neue", sans-serif`,
+  link_color                    : "#1d63ea",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#000000",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#1d63ea",
+  feature_icons_background_color: "#e6e6e6",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#666666",
+}
